252fdfb0edd1175de894c7c81c9abfad4d8d729b
Bavathaarani <sribavi472@gmail.com>
2022-10-11 09:12:15 +0000

    qwerty

---
+++ junit/junit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /home/coder/project/workspace/junit/test /home/coder/project/workspace/Testing-with-Selenium-TestNg/src/;
+cd /home/coder/project/workspace/Testing-with-Selenium-TestNg/;
+mvn clean test
